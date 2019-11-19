36f873584415200f856e5526c70322d3021311c0
Mboulgour <mathieu.r.77@gmail.com>
2019-11-19 17:28:29 +0100

    add loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Nav from './components/Nav';
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+    return () => clearTimeout(timer);
+  });
+
   return (
     <div>
-      <Nav />
+      {loading ? 
+        "Loading..."
+      :
+        <Nav />
+      }
     </div>
   );
 }
